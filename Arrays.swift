76674db7224216eae00b2f9fcd1598384638932e
A-f-Adib <a.f.adib36@gmail.com>
2024-05-22 03:47:56 +0600

    first unique element

--- Arrays.swift
+++ Arrays.swift
@@ -213,8 +213,24 @@ func PalindromicString(_ str: String) -> Bool {
 
 //Q10. First unique character in a string
 
-func firstUniqueC(_ str : String) -> Bool {
-	
+func firstUniqueC(_ str : String) -> Int {
+	let arr = Array(str)
+	var dict = [Character : Bool ]()
+
+	for item in arr {
+		if dict[item] != nil {
+			dict[item] = false
+		} else {
+			dict[item] = true
+		}
+	}
+
+	for (index, item) in arr.enumerated() {
+		if dict[item] == true {
+			return index
+		}
+	}
+	return -1
 
 }
-print(firstUniqueC(""))
+print(firstUniqueC("abcbda"))
