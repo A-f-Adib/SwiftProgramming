43e72db470a1dd818f69ab13ce8d692e72307157
A-f-Adib <a.f.adib36@gmail.com>
2024-05-26 13:48:21 +0600

    loop added

--- LinkedList/MergeTwoList.swift
+++ LinkedList/MergeTwoList.swift
@@ -1,8 +1,21 @@
 func mergeTwoList(_ l1 : Node?, _l2: Node?) -> Node? {
 
     var list1 = l1
-    var list2 = _l2
-    
+    var list2 = l2
+
     var result : Node? = Node(data: -1)
     var head = result
+
+    while list1 != nil && list2 != nil {
+        if list1!.data < list2!.data {
+            result!.next = list1
+            list1 = list1!.next
+        } else {
+            result!.next = list2
+            list2 = list2!.next
+        }
+        result = result!.next
+    }
+
+    
 }
