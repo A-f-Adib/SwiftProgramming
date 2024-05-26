dcb2f33aa0ae5598fa4efece603085d8be8e5e17
A-f-Adib <a.f.adib36@gmail.com>
2024-05-26 22:45:14 +0600

    DetectCycle

--- LinkedList/DetectCycle.swift
+++ LinkedList/DetectCycle.swift
@@ -8,3 +8,21 @@ public class ListNode {
     }
  }
 
+ class Solution {
+    func hasCycle(_ head: ListNode?) -> Bool {
+         var slow = head  
+         var fast = head 
+
+        while fast != nil && fast?.next != nil {
+            slow = slow?.next  
+            fast = fast?.next?.next 
+
+            if slow === fast {
+                return true  
+            }
+        }
+
+        return false  
+    }
+}
+
