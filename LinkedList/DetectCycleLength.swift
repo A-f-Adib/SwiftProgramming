1547c05d6e16bf2519a638cd4076c7fe0e457690
A-f-Adib <a.f.adib36@gmail.com>
2024-05-27 17:39:22 +0600

    modified

--- LinkedList/DetectCycleLength.swift
+++ LinkedList/DetectCycleLength.swift
@@ -21,11 +21,22 @@ public class ListNode {
             repeat {
                 count += 1
                 slow = slow!.next
-            } while  slow === fast
+            } while  slow!.val != fast!.val
 
-            print(count: \(count))
+            print("count: \(count)")
             return true
         }
     }
     return false
  }
+
+var list = ListNode(2)
+list.next = ListNode(3)
+let four = ListNode(4)
+list.next!.next = four
+list.next!.next!.next = ListNode(5)
+list.next!.next!.next!.next = ListNode(6)
+list.next!.next!.next!.next!.next = four
+
+var result = detectCycleLength(list)
+print(result)
