103e9cdbc31f3a4786704e4e2a90d606f691e167
A-f-Adib <a.f.adib36@gmail.com>
2024-05-27 17:31:25 +0600

    main func added

--- LinkedList/DetectCycleLength.swift
+++ LinkedList/DetectCycleLength.swift
@@ -6,3 +6,26 @@ public class ListNode {
         self.next = nil
     }
  }
+
+ func detectCycleLength(_ head: ListNode?) -> Bool {
+    var slow = head
+    var fast = head
+
+    while fast != nil && fast!.next != nil {
+        slow = slow!.next
+        fast = fast!.next!.next
+
+        if slow === fast {
+            var count = 0
+
+            repeat {
+                count += 1
+                slow = slow!.next
+            } while  slow === fast
+
+            print(count: \(count))
+            return true
+        }
+    }
+    return false
+ }
