b6bdd3afcc49e8cad204a04be862be1a2fe1525f
A-f-Adib <a.f.adib36@gmail.com>
2024-05-28 23:00:44 +0600

    modified

--- LinkedList/AddTwoNumber.swift
+++ LinkedList/AddTwoNumber.swift
@@ -1,10 +1,11 @@
+
 func addTwoNum(_ num1: ListNode?, _ num2: ListNode?) -> ListNode? {
     var l1 = num1
     var l2 = num2
 
     var carry = 0
     var result : ListNode? = ListNode(-1)
-    var head = result
+    let head = result
 
     while l1 != nil || l2 != nil {
 
@@ -37,4 +38,23 @@ public class ListNode {
         self.val = val
         self.next = nil
     }
+ }
+
+ //InPut: 
+
+ let num1 = ListNode(7)
+ num1.next = ListNode(5)
+ num1.next!.next = ListNode(9)
+ num1.next!.next!.next = ListNode(4)
+ num1.next!.next!.next!.next = ListNode(6)
+
+ let num2 = ListNode(8)
+ num2.next = ListNode(4)
+
+ //OutPut:
+ let head = addTwoNum(num1, num2)
+ var curr = head
+ while curr != nil {
+    print(curr!.val)
+    curr = curr!.next
  }
