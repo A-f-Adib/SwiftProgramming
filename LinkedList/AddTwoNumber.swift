d7918e266a785a0a357ae9449e3ee1d58c6c964d
A-f-Adib <a.f.adib36@gmail.com>
2024-05-28 22:44:23 +0600

    Add Two Numbers

---
+++ LinkedList/AddTwoNumber.swift
@@ -0,0 +1,11 @@
+func addTwoNum(_ num1: ListNode?, _ num2: ListNode?) -> ListNode? {
+    var l1 = num1
+    var l2 = num2
+
+    var carry = 0
+    var result : ListNode? = ListNode(-1)
+    var head = result
+
+
+    return head!.next
+}
