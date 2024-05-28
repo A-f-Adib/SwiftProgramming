3160655a8b21801abc13901c9d0341db3ba9e234
A-f-Adib <a.f.adib36@gmail.com>
2024-05-28 22:48:26 +0600

    func modified

--- LinkedList/AddTwoNumber.swift
+++ LinkedList/AddTwoNumber.swift
@@ -6,6 +6,18 @@ func addTwoNum(_ num1: ListNode?, _ num2: ListNode?) -> ListNode? {
     var result : ListNode? = ListNode(-1)
     var head = result
 
+    while l1 != nil || l2 != nil {
+
+        if l1 != nil {
+            carry = carry + l1!.val
+            l1 = l1!.next
+        }
+
+        if l2 != nil {
+            carry = carry + l2!.val
+            l2 = l2!.next
+        }
+    }
 
     return head!.next
 }
