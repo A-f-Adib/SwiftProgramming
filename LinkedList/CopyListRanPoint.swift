3036f3c4bf83c67ddea270d7d2b6a9fe669675fc
A-f-Adib <a.f.adib36@gmail.com>
2024-05-31 20:54:39 +0600

    modified

--- LinkedList/CopyListRanPoint.swift
+++ LinkedList/CopyListRanPoint.swift
@@ -14,5 +14,37 @@
 class Solution {
     func copyRandomList(_ head: Node?) -> Node? {
         
+
+        var curr = head
+        var newNode : Node?
+
+        while curr != nil {
+            newNode = curr.val
+            newNode.next = curr.next
+            curr.next = newNode
+            curr = newNode.next
+        }
+
+        curr = head
+        while curr != nil {
+            if curr.random != nil {
+                curr.next.random = curr.random.next
+                curr = curr.next.next
+            }
+        }
+
+        curr = head
+        var newHead : Node? = head.next
+        var newCurr = newHead
+        while curr != nil {
+            curr.next = newCurr!.next
+            curr = curr.next
+             if curr != nil {
+                newCurr.next = curr.next
+                newCurr = newCurr.next
+             }
+        }
+
+        return newHead
     }
 }
