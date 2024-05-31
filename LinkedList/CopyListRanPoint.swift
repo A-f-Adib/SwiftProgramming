3b845b8dca401c26442189c62cbbad36e6ffdcc0
A-f-Adib <a.f.adib36@gmail.com>
2024-05-31 20:41:27 +0600

    copy list with random pointer

--- LinkedList/CopyListRanPoint.swift
+++ LinkedList/CopyListRanPoint.swift
@@ -1,9 +1,18 @@
 
-public class ListNode {
-    public var val: Int
-     public var next: ListNode?
+ public class Node {
+     public var val: Int
+     public var next: Node?
+     public var random: Node?
      public init(_ val: Int) {
-        self.val = val
-        self.next = nil
+         self.val = val
+         self.next = nil
+    	 self.random = nil
+    }
+ }
+
+
+class Solution {
+    func copyRandomList(_ head: Node?) -> Node? {
+        
     }
 }
