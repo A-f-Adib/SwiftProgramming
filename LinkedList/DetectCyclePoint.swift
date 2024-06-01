8fbcbf2d4edca1c5dd2652f9073f9837fa839216
A-f-Adib <a.f.adib36@gmail.com>
2024-06-01 23:37:57 +0600

    modified

--- LinkedList/DetectCyclePoint.swift
+++ LinkedList/DetectCyclePoint.swift
@@ -1,5 +1,12 @@
 
-
+public class ListNode {
+    public var val: Int
+     public var next: ListNode?
+     public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+ }
 
 func detectCyclePoint(_ head: ListNode?) -> ListNode?  {
    
