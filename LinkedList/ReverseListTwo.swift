94caba496295857a34ee6ab131b1d0a45907a7e6
A-f-Adib <a.f.adib36@gmail.com>
2024-06-02 23:35:45 +0600

    modified

--- LinkedList/ReverseListTwo.swift
+++ LinkedList/ReverseListTwo.swift
@@ -1,13 +1,3 @@
 
 
-  public class ListNode {
-      public var val: Int
-      public var next: ListNode?
-      public init() { self.val = 0; self.next = nil; }
-      public init(_ val: Int) { self.val = val; self.next = nil; }
-      public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
-  }
- 
-    func reverseBetween(_ head: ListNode?, _ left: Int, _ right: Int) -> ListNode? {
-        
-    }
+  
