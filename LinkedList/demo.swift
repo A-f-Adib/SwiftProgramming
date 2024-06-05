8ab1513509f47be63d7cbd1aba3e2eec48a03ca5
A-f-Adib <a.f.adib36@gmail.com>
2024-06-06 03:05:46 +0600

    updated

---
+++ LinkedList/demo.swift
@@ -0,0 +1,46 @@
+
+  public class ListNode {
+      public var val: Int
+      public var next: ListNode?
+      public init() { self.val = 0; self.next = nil; }
+      public init(_ val: Int) { self.val = val; self.next = nil; }
+      public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ }
+ 
+func reverseBetween(_ head: ListNode?, _ left: Int, _ right: Int) -> ListNode? {
+        
+        if head == nil {
+            return head
+        }
+        var dummyNode : ListNode? = ListNode(-1)
+        dummyNode?.next = head
+
+ var leftPrev = dummyNode
+ var curr = head
+
+ for _ in 0..<left - 1 {
+    leftPrev = leftPrev?.next
+    curr = curr?.next
+ }
+
+   let subListHead = curr
+    var preNode : ListNode? = nil
+
+    for _ in 0...right - left {
+      let nextNode = curr?.next
+      curr?.next = preNode
+      preNode = curr
+      curr = nextNode
+    }
+    leftPrev?.next = preNode
+    subListHead?.next = curr
+
+    return dummyNode?.next
+      
+          
+}
+
+
+
+
+
--- LinkedList/removeNthNode.swift
+++ LinkedList/removeNthNode.swift
@@ -7,5 +7,34 @@
  }
 
 func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+        let dummy : ListNode? = ListNode(-1)
+        dummy?.next = head
         
+        var firstPtr = dummy
+        var secondPtr = dummy
+
+        for _ in 0..<n {
+            secondPtr = secondPtr?.next
+        }
+
+        while secondPtr?.next != nil {
+            firstPtr = firstPtr?.next
+            secondPtr = secondPtr?.next
+        }
+
+        firstPtr?.next = firstPtr?.next?.next
+        return dummy?.next
 }
+
+let num1 = ListNode(7)
+ num1.next = ListNode(5)
+ num1.next!.next = ListNode(9)
+ num1.next!.next!.next = ListNode(4)
+ num1.next!.next!.next!.next = ListNode(6)
+
+let head = removeNthFromEnd(num1, 3)
+ var curr = head
+ while curr != nil {
+    print(curr!.val)
+    curr = curr!.next
+ }
