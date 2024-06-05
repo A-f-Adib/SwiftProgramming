53a06d7d8c0952f39c7cb45fc5b4c6e026791452
A-f-Adib <a.f.adib36@gmail.com>
2024-06-05 22:44:49 +0600

    modified

--- LinkedList/ReverseListTwo.swift
+++ LinkedList/ReverseListTwo.swift
@@ -12,21 +12,30 @@ func reverseBetween(_ head: ListNode?, _ left: Int, _ right: Int) -> ListNode? {
          if head == nil {
             return head
         }
- var dummyNode : ListNode
- dummyNode.next = head
+var dummyNode : ListNode?
+ dummyNode?.next = head
 
  var leftPrev = dummyNode
  var curr = head
 
- for i in 0..<left - 1 {
-    leftPrev = leftPrev.next
-    curr = curr.next
+ for _ in 0..<left - 1 {
+    leftPrev = leftPrev?.next
+    curr = curr?.next
  }
    
-   var subListHead = curr
+   let subListHead = curr
     var preNode : ListNode? = nil
-    
 
+    for _ in 0...right - left {
+      let nextNode = curr?.next
+      curr?.next = preNode
+      preNode = curr
+      curr = nextNode
+    }
+    leftPrev?.next = preNode
+    subListHead?.next = curr
+
+    return dummyNode?.next
 
       
     }
