a933beae5113196eee3fad321eda21ab00d793da
A-f-Adib <a.f.adib36@gmail.com>
2024-06-10 23:32:46 +0600

    modified

--- Arrays.swift
+++ Arrays.swift
@@ -314,14 +314,23 @@ func internalFunc(_ arr : [Character], _ start : Int, _ end : Int, _ removed : B
 
  //sol-3:
    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-        var countOfNums1 = m
+      
+	    var countOfNums1 = m
         var countOfNums2 = n
+
 		 var leftPointer = m - 1
         var rightPointer = n - 1
+
         var mergePointer = m + n - 1
+
 		 while leftPointer >= 0 || rightPointer >= 0 {
+
             if leftPointer >= 0 && (rightPointer < 0 || nums1[leftPointer] > nums2[rightPointer]) {
+              
+			   nums1[mergePointer] = nums1[leftPointer]
                
+			    leftPointer -= 1
+            } else {
    }
 //------------------------------------------------------------------------------------
 
