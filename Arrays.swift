882833c52cf34f9a151d69ad4c7927738eaf326e
A-f-Adib <a.f.adib36@gmail.com>
2024-06-11 23:47:13 +0600

    modified

--- Arrays.swift
+++ Arrays.swift
@@ -312,12 +312,6 @@ func internalFunc(_ arr : [Character], _ start : Int, _ end : Int, _ removed : B
     }
  }
 
- //sol-3:
-   func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-      
-	    var countOfNums1 = m
-        var countOfNums2 = n
-
 		 var leftPointer = m - 1
         var rightPointer = n - 1
 
