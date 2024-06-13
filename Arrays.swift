b0fa78ca99f620a2f3228a327fe87154db95b2a8
A-f-Adib <a.f.adib36@gmail.com>
2024-06-13 23:13:01 +0600

    modified

--- Arrays.swift
+++ Arrays.swift
@@ -314,8 +314,6 @@ func internalFunc(_ arr : [Character], _ start : Int, _ end : Int, _ removed : B
     }
  }
 
- func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-        var lastA = m - 1
         var lastB = n - 1
         var curr = nums1.count - 1
 
