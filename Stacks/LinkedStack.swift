69a91ec7ceeec393d0b282afeea81be0ee3d7a36
A-f-Adib <a.f.adib36@gmail.com>
2024-06-15 03:19:32 +0600

    func added

--- Stacks/LinkedStack.swift
+++ Stacks/LinkedStack.swift
@@ -8,5 +8,17 @@ class Node {
 }
 
 class Stack {
-    
+    private var head : Node?
+    private var length : Int
+
+    init() {
+        self.length = 0
+    }
+
+    func push(_ data : Int) {
+        let temp = head
+        head = Node(data)
+        head?.next = temp
+        length += 1
+    }
 }
