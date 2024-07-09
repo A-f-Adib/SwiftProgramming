0cb288cc4820a9bb9b64776eb0d9f0152bc683f6
A-f-Adib <a.f.adib36@gmail.com>
2024-07-09 22:52:14 +0600

    Rotate Array (Right Rotation)

--- Arrays/RotateArray.swift
+++ Arrays/RotateArray.swift
@@ -3,13 +3,34 @@
  //Step-3: Reverse last length-k element
 
  func rotate(_ nums:  [Int], _ k: Int) -> [Int] {
-        
-  let n = nums.count
 
+  var nums = nums    
+  let n = nums.count
+  var arr = [Int]()
+
+      reverse(&nums, 0, n-1)
+      reverse(&nums, 0, k-1)
+      reverse(&nums, k, n-1)
+
+      for i in nums {
+         arr.append(i)
+      }
+
+      return arr
 }
 
 func reverse(_ nums: inout [Int], _ start : Int, _ end: Int) {
-    
+    var start = start
+    var end  = end
+
+    while  start < end {
+        let temp = nums[start]
+        nums[start] = nums[end]
+        nums[end] = temp
+
+        start += 1
+        end -= 1
+    }
 }
 
 print(rotate([1,2,3,4], 1))
