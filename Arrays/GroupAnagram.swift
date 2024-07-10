e5fc182db8b4cfcdc471a95594a6cb6b9c4f949f
A-f-Adib <a.f.adib36@gmail.com>
2024-07-10 06:35:08 +0600

    GroupAnagram

--- Arrays/GroupAnagram.swift
+++ Arrays/GroupAnagram.swift
@@ -1,6 +1,26 @@
  
  func groupAnagrams(_ strs: [String]) -> [[String]] {
+        
         if strs.isEmpty {
             return []
         }
+
+          var holder = [String: [String]]()
+
+        for str in strs {
+            let sortedStr = String(str.sorted())
+
+            if holder[sortedStr] == nil {
+                holder[sortedStr] = [str]
+            } 
+            else {
+                var prevCombo = holder[sortedStr]!
+                prevCombo.append(str)
+                holder[sortedStr] = prevCombo
+            }
+        }
+        return Array(holder.values)
  }
+
+ //Test Cases:
+ print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
