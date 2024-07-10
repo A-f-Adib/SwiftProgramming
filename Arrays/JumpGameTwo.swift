31c3c739785e9565c8b050f3a22e2ca03613830d
A-f-Adib <a.f.adib36@gmail.com>
2024-07-10 06:06:57 +0600

    JumpGameTwo

---
+++ Arrays/JumpGameTwo.swift
@@ -0,0 +1,31 @@
+func canJumpTwo(_ nums: [Int]) -> Int {
+   
+   var totalJumps = 0
+   var coverage = 0
+   var lastJump = 0
+   let destination = nums.count - 1
+
+   if nums.count == 1 {
+    return 0
+   }
+
+   for i in 0..<nums.count {
+
+        coverage = max(coverage, i + nums[i])
+
+        if i == lastJump {
+            lastJump = coverage
+            totalJumps += 1
+
+            if coverage >= destination {
+                return totalJumps
+            }
+        }
+   }
+
+   return totalJumps
+}
+
+//Test cases:
+print(canJumpTwo([1,2,3,0,5]))
+print(canJumpTwo([3,2,1,0,5]))
