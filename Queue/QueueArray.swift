cb4417f242dcdda5651b93ea52351115338466d0
A-f-Adib <a.f.adib36@gmail.com>
2024-07-27 03:37:59 +0600

    Queue Array

---
+++ Queue/QueueArray.swift
@@ -0,0 +1,17 @@
+
+struct  QueueArray {
+
+    private var arr = [Int]()
+
+    var isEmpty : Bool {
+        return arr.isEmpty
+    }
+
+    var peek : Int? {
+        return arr.first
+    }
+
+    init() {
+        arr[]
+    }
+}
