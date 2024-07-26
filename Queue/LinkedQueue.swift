15d946b5acdb4605f3b97267150c4c9361c3000d
A-f-Adib <a.f.adib36@gmail.com>
2024-07-27 04:13:34 +0600

    func added

--- Queue/LinkedQueue.swift
+++ Queue/LinkedQueue.swift
@@ -11,5 +11,26 @@ class LinkedQueue {
     private var head : Node?
     private var tail : Node?
     private var length = 0
-    
+
+    func enqueue(_ data : Int) {
+        length += 1
+        let node = Node(data)
+        if head == nil {
+            head = node
+            tail = node
+            return
+        }
+        tail!.next = node
+        tail = node
+    }
+
+    func dequeue() -> Node? {
+        if head == nil {
+            return nil
+        }
+        length -= 1
+        let node = head
+        head = head!.next
+        return node
+    }
 }
