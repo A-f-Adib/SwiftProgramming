85a1c1df97214bdfff6ab23b221147a3a1d7fe0a
A-f-Adib <a.f.adib36@gmail.com>
2024-07-29 23:24:52 +0600

    Longest common prefix

--- Arrays/LCPrefix.swift
+++ Arrays/LCPrefix.swift
@@ -1,7 +1,15 @@
  
   func longestCommonPrefix(_ strs: [String]) -> String {
+    
         guard strs.count > 0 else { return "" }
         let chars_array = strs.map({ Array($0) })
         var string = ""
         var i = 0
+
+          while true {
+            var c: Character? = nil
+            for chars in chars_array {
+                if i >= chars.count {
+                    return string
+                }
   }
