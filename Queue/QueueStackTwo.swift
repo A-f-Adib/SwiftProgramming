05359ffff4fe9588ac675ae0cd3b737e9ee14271
A-f-Adib <a.f.adib36@gmail.com>
2024-07-30 22:51:52 +0600

    QueueStackTwo

--- Queue/QueueStackTwo.swift
+++ Queue/QueueStackTwo.swift
@@ -8,3 +8,18 @@ class Node<T> {
         self.data = data
     }
 }
+
+class Stack<T> {
+    var head : Node<T>?
+    var count = 0
+    var isEmpty: Bool {
+        return count == 0
+    }
+
+    func push(_ data : T) {
+        count += 1
+        let node = Node(data : data)
+        if head == nil {
+            head = node
+            return
+        }
