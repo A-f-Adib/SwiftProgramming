ea557675e32ac0da39b7b51b127ea7e06624f502
A-f-Adib <a.f.adib36@gmail.com>
2024-07-31 05:07:44 +0600

    UniqueEmail

--- Arrays/UniqueEmail.swift
+++ Arrays/UniqueEmail.swift
@@ -1,3 +1,4 @@
+import Foundation
 
 func getUniqueEmail(_ list: [String]) -> Int {
     if list.isEmpty {
@@ -10,8 +11,14 @@ func getUniqueEmail(_ list: [String]) -> Int {
     for item in list {
         let emailPart = item.split(separator: "@").map {String($0)}
         var localName = emailPart[0]
-        var domainName = emailPart[1]
+        let domainName = emailPart[1]
 
-        
+        localName = localName.replacingOccurrences(of: ".", with: "")
+        if let idx = localName.range(of: "+") {
+            localName = String(localName[..<idx.lowerBound])
+        }
+        uniqueEmails.insert("\(localName)@\(domainName)")
     }
+    print(uniqueEmails)
+    return uniqueEmails.count
 }
