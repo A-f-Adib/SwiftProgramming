a7e2e599306aab41169b029cd5136dbe9aff8a48
A-f-Adib <a.f.adib36@gmail.com>
2024-08-12 22:52:08 +0600

    FirstOccurance

---
+++ Arrays/FirstOcc.swift
@@ -0,0 +1,7 @@
+ func strStr(_ haystack: String, _ needle: String) -> Int {
+        var n = Array(needle)
+        var h = Array(haystack)
+        var l = 0
+        var r = 0
+        var res = -1
+ }
