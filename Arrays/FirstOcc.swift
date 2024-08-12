af8e78af1c45207f05dc6bed4493191049a08b2d
A-f-Adib <a.f.adib36@gmail.com>
2024-08-12 22:53:25 +0600

    First Occurane

--- Arrays/FirstOcc.swift
+++ Arrays/FirstOcc.swift
@@ -4,4 +4,12 @@
         var l = 0
         var r = 0
         var res = -1
+
+    guard n.count <= h.count else { return -1 } 
+
+        while l < h.count{
+            if h[l] == n[0]{
+                res = l
+                r = l + 1
+                var j = 1
  }
