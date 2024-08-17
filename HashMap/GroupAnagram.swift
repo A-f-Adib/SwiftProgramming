9217a3bb744516bf1eac41e8c22c7ec4e6737c4f
A-f-Adib <a.f.adib36@gmail.com>
2024-08-18 05:32:25 +0600

    GroupAnagram

--- HashMap/GroupAnagram.swift
+++ HashMap/GroupAnagram.swift
@@ -1,10 +1,21 @@
 
     func groupAnagrams(_ strs: [String]) -> [[String]] {
+       
         if strs.isEmpty {
             return []
         }
 
         var holder = [String: [String]]()
+
         for str in strs {
-         
+            let sortedStr = String(str.sorted())
+            if holder[sortedStr] == nil {
+                holder[sortedStr] = [str]
+            } else {
+                var prevCombo = holder[sortedStr]!
+                prevCombo.append(str)
+                holder[sortedStr] = prevCombo
+            }
+        }
+        return Array(holder.values)
     }
