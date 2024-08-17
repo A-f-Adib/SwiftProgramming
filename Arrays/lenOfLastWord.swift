f742d13315263bd0db41918de428c29fe8deff83
A-f-Adib <a.f.adib36@gmail.com>
2024-08-17 23:14:19 +0600

    length of last word

---
+++ Arrays/lenOfLastWord.swift
@@ -0,0 +1,15 @@
+ 
+ func lengthOfLastWord(_ s: String) -> Int {
+        
+        var ans = 0
+
+        for c in s.reversed() {
+            if c != " " {
+                ans += 1
+            } else if ans > 0 {
+                return ans
+            }
+        }
+
+        return ans
+}
