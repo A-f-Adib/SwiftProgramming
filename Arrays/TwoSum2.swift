ffe26795c193c952f78a50b03205bc5c6d528157
A-f-Adib <a.f.adib36@gmail.com>
2024-08-17 23:47:56 +0600

    TwoSum2

--- Arrays/TwoSum2.swift
+++ Arrays/TwoSum2.swift
@@ -13,4 +13,29 @@ func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
     }
     return []
 }
+
+//Sorted
+ func twoSum2(_ numbers: [Int], _ target: Int) -> [Int] {
+         
+	 var dict = [Int: Int]()
+
+        for i in 0..<numbers.count {
+            let diff = target - numbers[i]
+
+          
+            if let index1 = dict[diff] {
+                return [index1 + 1, i + 1]
+            } else {
+                dict[numbers[i]] = i
+            }
+        }
+
+        return []
+}
+
+//test cases:
+
  print(twoSum([1,5,4], 6))
+ print(twoSum2([1,4,5], 6))
+ print(twoSum([2,7,11,15], 13))
+ print(twoSum2([2,7,11,15], 13))
