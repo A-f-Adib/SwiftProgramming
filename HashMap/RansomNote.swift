5b102a8be1a52bfe5b7a15cdbda6fe4b33acc7e3
A-f-Adib <a.f.adib36@gmail.com>
2024-08-20 23:40:51 +0600

    solution 2

--- HashMap/RansomNote.swift
+++ HashMap/RansomNote.swift
@@ -14,11 +14,29 @@
         return true            
  }
 
+ //sol-2:
+  func canConstruct2(_ ransomNote: String, _ magazine: String) -> Bool {
+        var charCount = [Character: Int]()
+
+        for char in magazine {
+            charCount[char, default: 0] += 1
+        }
+        
+        for char in ransomNote {
+            if let count = charCount[char], count > 0 {
+                charCount[char] = count - 1
+            } else {
+                return false
+            }       
+        }
+        return true
+}
+
  //test cases:
 
-print(canConstruct("aa", "ab"))
-print(canConstruct("aa", "aa"))
-print(canConstruct("aa", "a"))
-print(canConstruct("aab", "abb"))
-print(canConstruct("aba", "aab"))
+print(canConstruct2("aa", "ab"))
+print(canConstruct2("aa", "aa"))
+print(canConstruct2("aa", "a"))
+print(canConstruct2("aab", "abb"))
+print(canConstruct2("aba", "aab"))
     
