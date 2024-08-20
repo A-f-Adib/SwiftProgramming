afa87b7a87c143de81e79be1ce6938aa1273d7bf
A-f-Adib <a.f.adib36@gmail.com>
2024-08-20 23:17:02 +0600

    RansomNote

--- HashMap/RansomNote.swift
+++ HashMap/RansomNote.swift
@@ -1,4 +1,8 @@
  
  func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
         var magazineCopy = magazine
+
+          for letter in ransomNote {
+            if let index = magazineCopy.firstIndex(of: letter) {
+                magazineCopy.remove(at: index)
  }
