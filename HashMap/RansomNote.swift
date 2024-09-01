a554653bdac8b5fd5aec6e7d507be2b82b456108
A-f-Adib <a.f.adib36@gmail.com>
2024-09-01 23:02:31 +0600

    modified

--- HashMap/RansomNote.swift
+++ HashMap/RansomNote.swift
@@ -1,14 +1,5 @@
 
-//          } else {
-//                 return false
-//             }
-//         }
-
-//         return true            
-//  }
-
- //sol-2:
-  func canConstruct2(_ ransomNote: String, _ magazine: String) -> Bool {
+  func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
         var charCount = [Character: Int]()
 
         for char in magazine {
@@ -27,9 +18,9 @@
 
  //test cases:
 
-print(canConstruct2("aa", "ab"))
-print(canConstruct2("aa", "aa"))
-print(canConstruct2("aa", "a"))
-print(canConstruct2("aab", "abb"))
-print(canConstruct2("aba", "aab"))
+print(canConstruct("aa", "ab"))
+print(canConstruct("aa", "aa"))
+print(canConstruct("aa", "a"))
+print(canConstruct("aab", "abb"))
+print(canConstruct("aba", "aab"))
     
