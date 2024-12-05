2bea8945834f27ed15d0365bab9cc07a05daebb5
A.F. Adib <a.f.adib36@gmail.com>
2024-12-05 22:52:14 +0600

    HIndex

--- Arrays/Hindex.swift
+++ Arrays/Hindex.swift
@@ -1,39 +1,17 @@
 
-func hIndex(_ citations: [Int]) -> Int {
+    func hIndex(_ citations: [Int]) -> Int {
+        
+        var finalValue = 0
+        let sortedArray : [Int] = citations.sorted(by: >)
 
-        let sortedCitations = citations.sorted {$0 > $1}
-
-        var i = 0 
-        var hIndex = 0 
-
-    while i < sortedCitations.count {
-
-        if sortedCitations[i] > i {
-                hIndex = i + 1
+        for (index, value) in sortedArray.enumerated() {
+            if value >= index + 1 {
+                finalValue = index + 1
+               
+            } 
         }
-            i += 1
-        }
+         return finalValue
+    }
 
-        return hIndex
-}
-
-//Sol-2:
-
- func hIndex(_ citations: [Int]) -> Int {
-    
-        let hIn = citations.sorted(by: >)
-        var h = 0
-
-        for (i, cit) in hIn.enumerated() {
-
-            if i + 1 <= cit {
-                h = i + 1
-            } else {
-                return h
-            }
-        }
-
-        return h
-}
-  
-  //-----------------------------------------------------------------------------------------
+// Output:
+print(hIndex([3,0,7,2,5]))
