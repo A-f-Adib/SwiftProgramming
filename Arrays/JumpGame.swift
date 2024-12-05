71a81dd711b0d148772ae473fb33cec45353b830
A.F. Adib <a.f.adib36@gmail.com>
2024-12-05 23:06:40 +0600

    canJump

--- Arrays/JumpGame.swift
+++ Arrays/JumpGame.swift
@@ -1,7 +1,21 @@
   
 func canJump(_ nums: [Int]) -> Bool {
     
+    var farthest = 0
     
+    for (index, value) in nums.enumerated() {
+
+     if  index > farthest {
+          return false
+     }
+
+     farthest = max(farthest, index + value)
+
+     if farthest >= nums.count - 1 {
+          return true
+     }
+    }
+    return true
 }
 
 //Test cases:
