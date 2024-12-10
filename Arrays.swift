56fa10dce4b1dbba58d49e55caecfd43d5480f6d
A.F. Adib <a.f.adib36@gmail.com>
2024-12-10 13:42:01 +0600

    sorted

--- Arrays.swift
+++ Arrays.swift
@@ -1,19 +1,5 @@
 //Q1. two sum problem
 
-func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-    
-	for i in 0..<(nums.count - 1) {
-        let ni = nums[i]
-
-	for j in (i + 1)..<nums.count   {
-	    if ni + nums[j] == target {
-	        return [i, j]
-	    }
-	}
-    }
-    return []
-}
-//  print(twoSum([1,5,4], 6))
 
  //-------------------------------------------------------------------------------------------
 //Q2. Contains duplicate (217)
