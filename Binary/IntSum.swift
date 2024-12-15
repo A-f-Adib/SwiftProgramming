1c164eaf74651e8ef3084104d0cb75b71161b89e
A.F. Adib <a.f.adib36@gmail.com>
2024-12-15 19:26:57 +0600

    practice

--- Binary/IntSum.swift
+++ Binary/IntSum.swift
@@ -1,1 +1,9 @@
-  
+ func getSum(_ a: Int, _ b: Int) -> Int {
+      
+        var x = abs(a)
+        var y = abs(b)
+        if x < y { return getSum(b, a) }
+        let sign = a > 0 ? 1 : -1
+        let addition: Bool = a * b > 0
+
+ }    
