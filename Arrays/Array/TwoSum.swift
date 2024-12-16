e8bd989968a6224245cfa5bb5d87a164dcf8aa65
A.F. Adib <a.f.adib36@gmail.com>
2024-12-17 01:19:28 +0600

    practice

--- Arrays/Array/TwoSum.swift
+++ Arrays/Array/TwoSum.swift
@@ -1,17 +1,22 @@
 
 
 func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-    
-	for i in 0..<(nums.count - 1) {
-        let ni = nums[i]
 
-	for j in (i + 1)..<nums.count   {
-	    if ni + nums[j] == target {
-	        return [i, j]
-	    }
-	}
+    var numMap = [Int: Int]()
+    
+    for i in 0..<nums.count {
+
+        let complement = target - nums[i] 
+        
+        if let index = numMap[complement] { // Check if the complement is already in the map
+
+            return [index, i] // Return the indices of the two numbers
+        }
+        
+        numMap[nums[i]] = i // Store the current number and its index
     }
-    return []
+    
+    return [] 
 }
 
  print(twoSum([1,5,4], 6))
