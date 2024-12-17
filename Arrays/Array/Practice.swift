e3bfe541cb8c253e5f8ccfc237178ca1bcccb93a
A.F. Adib <a.f.adib36@gmail.com>
2024-12-17 23:53:14 +0600

    prctce

--- Arrays/Array/Practice.swift
+++ Arrays/Array/Practice.swift
@@ -1,14 +1,2 @@
 
-   func containsDuplicate(_ nums: [Int]) -> Bool {
-    
-    let sorted = nums.sorted()
-
-    for i in 0..<sorted.count {
-        if sorted[i] == sorted[i + 1] {
-            return true
-        }
-    }
-    return false
-    
-    }
-    print(containsDuplicate([7,1,5,3,6,4]))
+   
