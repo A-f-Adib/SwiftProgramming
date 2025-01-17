d4955f1e34f00247f443fa48d11f862687c4e658
A.F. Adib <a.f.adib36@gmail.com>
2025-01-17 22:54:17 +0600

    single element in sorted array

--- Arrays/Searching/SinElementSortArr.swift
+++ Arrays/Searching/SinElementSortArr.swift
@@ -19,3 +19,23 @@
         if nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1] {
             return nums[mid]
         }
+
+        if mid % 2 == 0 {
+            if nums[mid] == nums[mid - 1] {
+                end = mid - 1
+            } else {
+                start = mid + 1
+            }
+        } else {
+            if nums[mid] == nums[mid - 1 ] {
+                start = mid + 1
+            } else {
+                end = mid - 1
+            }
+        }
+    }    
+     return -1   
+ }
+
+ //Test cases:
+ print(singleNonDuplicate([1,1,2,3,3,4,4,8,8]))
