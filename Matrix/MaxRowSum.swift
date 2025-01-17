5aa5ba03317ea561eedfd6fc298adc7794ae03e2
A.F. Adib <a.f.adib36@gmail.com>
2025-01-18 01:36:32 +0600

    max col sum

--- Matrix/MaxRowSum.swift
+++ Matrix/MaxRowSum.swift
@@ -13,3 +13,23 @@ func maxRowSum(_ matrix : [[Int]]) -> Int {
     return maxSum
 }
 
+
+func maxColSum(_ matrix : [[Int]]) -> Int {
+
+    var maxSum = 0
+
+    for j in 0..<matrix[0].count {
+        var colSum = 0
+        for i in 0..<matrix.count {
+            colSum += matrix[i][j]
+        }
+        maxSum = max(colSum, maxSum)
+    }
+    return maxSum
+}
+
+//Test Cases:
+print("Max row sum:")
+print(maxRowSum([[100,2,30],[4,5,6],[7,8,9]]))
+print("Max column sum:")
+print(maxColSum([[100,2,30],[4,5,6],[7,8,9]]))
