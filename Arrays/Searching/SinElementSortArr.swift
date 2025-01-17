34720f71ad50f8d4cc59431f4bf9c284775ff0b0
A.F. Adib <a.f.adib36@gmail.com>
2025-01-17 22:53:25 +0600

    single element in sorted array

--- Arrays/Searching/SinElementSortArr.swift
+++ Arrays/Searching/SinElementSortArr.swift
@@ -1,3 +1,21 @@
+//The arr size will always be odd
+//if mid is even index and mid == mid -1 then the element is in the left , else right
+//if mid is odd index and mid == mid - 1 then the element is in the right , else left
+//540
 
+ func singleNonDuplicate(_ nums: [Int]) -> Int {
 
-     
+    let n = nums.count
+    var start = 0
+    var end = n - 1
+
+    while start <= end {
+
+        let mid = start + (end - start) / 2
+
+        if mid == 0 && nums[mid] != nums[mid + 1] { return nums[mid] }
+        if mid == n - 1 && nums[n - 1] != nums[ n - 2] { return nums[mid] }
+
+        if nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1] {
+            return nums[mid]
+        }
