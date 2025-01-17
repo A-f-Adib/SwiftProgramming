43b6a794006fe33ba8c7caaf2cb3b0c5c018ca02
A.F. Adib <a.f.adib36@gmail.com>
2025-01-17 23:08:59 +0600

    diagonal sum func 2

--- Matrix/DiagonalSum.swift
+++ Matrix/DiagonalSum.swift
@@ -20,3 +20,26 @@ func diagonalSum(_ matrix : [[Int]], _ size: Int) -> Int {
     //return sum of two diagonals
     return totalSum
 }
+
+
+
+func diagonalSumOptimized(_ matrix : [[Int]], _ size: Int) -> Int {
+
+    var totalSum = 0
+
+    for i in 0..<matrix.count {
+
+        totalSum += matrix[i][i]
+
+        if i != size - 1 - i {
+            totalSum += matrix[i][size - i - 1]
+        }
+    }
+    //return sum of two diagonals
+    return totalSum
+}
+
+
+//test cases:
+print(diagonalSum([[1,2,3],[4,5,6],[7,8,9]], 3))
+print(diagonalSumOptimized([[1,2,3],[4,5,6],[7,8,9]], 3))
