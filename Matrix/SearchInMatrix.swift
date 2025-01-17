6f8eeac200429e74515f239cfdb44d810aec5ed2
A.F. Adib <a.f.adib36@gmail.com>
2025-01-18 01:38:16 +0600

    search in row

--- Matrix/SearchInMatrix.swift
+++ Matrix/SearchInMatrix.swift
@@ -22,3 +22,27 @@ func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
         }
         return false
 }
+
+
+func SearchInRow(_ matrix: [[Int]], _ target: Int, _ row: Int) -> Bool {
+
+    let n = matrix[0].count
+    var start = 0
+    var end = n - 1
+
+    while start <= end {
+        let mid = start + (end - start) / 2
+        
+        if target == matrix[row][mid] {
+            return true
+        }
+        else if target > matrix[row][mid] {
+            start = mid + 1
+        } else {
+            end = mid - 1
+        }
+    }
+    return false
+}
+
+print(searchMatrix([[1,2,3],[4,5,6],[7,8,9]], 32))
