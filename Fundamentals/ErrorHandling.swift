088862d606b967ab3d6d4871821f943e6c477384
A.F. Adib <a.f.adib36@gmail.com>
2025-02-05 04:36:38 +0600

    error hnd

--- Fundamentals/ErrorHandling.swift
+++ Fundamentals/ErrorHandling.swift
@@ -14,3 +14,26 @@ func withdraw(amount: Double, from accountBalance: Double) throws -> Double {
     }
     return accountBalance - amount
 }
+
+
+do {
+    let Balance = try withdraw(amount: 500, from: 5000)
+    print("New balance is: \(Balance)")
+} catch AccountError.insufficientFunds {
+    print("Error: You don't have enough funds!")
+} catch AccountError.accountNotFound {
+    print("Error: Account not found!")
+} catch {
+    print("Unexpected error: \(error)")
+}
+
+do {
+    let newBalance = try withdraw(amount: 100, from: 50)
+    print("New balance is: \(newBalance)")
+} catch AccountError.insufficientFunds {
+    print("Error: You don't have enough funds!")
+} catch AccountError.accountNotFound {
+    print("Error: Account not found!")
+} catch {
+    print("Unexpected error: \(error)")
+}
