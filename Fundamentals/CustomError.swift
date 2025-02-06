6c8c1d72a675278c4a92182a6a24f7a1e726323b
A.F. Adib <a.f.adib36@gmail.com>
2025-02-06 20:41:16 +0600

    custom error

---
+++ Fundamentals/CustomError.swift
@@ -0,0 +1,16 @@
+
+import Foundation 
+
+enum PaymentError: Error, LocalizedError {
+    case insufficientFunds
+    case networkError
+
+    var errorDescription: String? {
+        switch self {
+        case .insufficientFunds:
+            return "You don't have enough funds to complete this transaction."
+        case .networkError:
+            return "There was a problem connecting to the network. Please try again."
+        }
+    }
+}
