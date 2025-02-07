594723933d368cb1620be510b9c55390c8372e03
A.F. Adib <a.f.adib36@gmail.com>
2025-02-07 23:15:16 +0600

    practice

--- Arrays/Array/Practice.swift
+++ Arrays/Array/Practice.swift
@@ -1,2 +1,18 @@
 
    
+    func hIndex(_ citations: [Int]) -> Int {
+        
+        var finalValue = 0
+        let sortedArray : [Int] = citations.sorted(by: >)
+
+        for (index, value) in sortedArray.enumerated() {
+            if value >= index + 1 {
+                finalValue = index + 1
+               
+            } 
+        }
+         return finalValue
+    }
+
+// Output:
+print(hIndex([3,0,7,2,5]))
