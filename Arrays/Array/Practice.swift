04326f5775ef55253b4e9b5ab8ee3430110eaac5
A.F. Adib <a.f.adib36@gmail.com>
2025-02-07 23:17:05 +0600

    prctce

--- Arrays/Array/Practice.swift
+++ Arrays/Array/Practice.swift
@@ -14,5 +14,30 @@
          return finalValue
     }
 
+
+  
+func canJump(_ nums: [Int]) -> Bool {
+    
+    var farthest = 0
+    
+    for (index, value) in nums.enumerated() {
+
+     if  index > farthest {
+          return false
+     }
+
+     farthest = max(farthest, index + value)
+
+     if farthest >= nums.count - 1 {
+          return true
+     }
+    }
+    return true
+}
+
+//Test cases:
+print(canJump([1,2,3,0,5]))
+print(canJump([3,2,1,0,5]))
+
 // Output:
 print(hIndex([3,0,7,2,5]))
