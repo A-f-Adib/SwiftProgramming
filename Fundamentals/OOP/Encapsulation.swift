ca13667e737729c1da001fd4bbbd46969be3559f
A.F. Adib <a.f.adib36@gmail.com>
2025-02-22 16:31:02 +0600

    encapsulation

--- Fundamentals/OOP/Encapsulation.swift
+++ Fundamentals/OOP/Encapsulation.swift
@@ -8,6 +8,15 @@ class BankAccount {
         self.balance = initialBalance
     }
     
+    func deposit(amount: Double) {
+        guard amount > 0 else {
+            print("Deposit amount must be positive.")
+            return
+        }
+        balance += amount
+        print("Deposited \(amount). New balance: \(balance)")
+    }
 }
 
 let account = BankAccount(initialBalance: 1000)
+account.deposit(amount: 500)   // Deposited 500. New balance: 1500
