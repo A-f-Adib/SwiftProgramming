d0917ae5b076cad0c571ba877f0d2f08ed7c9cb5
A.F. Adib <a.f.adib36@gmail.com>
2025-02-22 16:47:57 +0600

    abstraction

--- Fundamentals/OOP/Abstraction.swift
+++ Fundamentals/OOP/Abstraction.swift
@@ -16,3 +16,27 @@ class PayPalPayment: Payment {
         print("Processing PayPal payment of $\(amount)")
     }
 }
+
+func makePayment(paymentMethod: Payment, amount: Double) {
+    paymentMethod.processPayment(amount: amount)
+}
+
+let creditCard = CreditCardPayment()
+let paypal = PayPalPayment()
+
+makePayment(paymentMethod: creditCard, amount: 200)  // Processing credit card payment of $200
+makePayment(paymentMethod: paypal, amount: 150)      // Processing PayPal payment of $150
+
+
+//-----------------------------------------------------------
+
+/*
+Abstraction is about exposing only the relevant parts of an object while hiding 
+unnecessary implementation details. Swift achieves this using protocols and access control modifiers.
+
+Here, 
+-> The Payment protocol abstracts payment processing behavior.
+-> Concrete classes (CreditCardPayment and PayPalPayment) implement the protocol without 
+   exposing their internal details.
+-> The makePayment function operates on the Payment protocol, achieving abstraction.
+*/
