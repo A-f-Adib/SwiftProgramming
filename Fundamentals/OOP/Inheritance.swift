d8d6b2cad28b79f757a3a8139a8af6c7ba05d2be
A.F. Adib <a.f.adib36@gmail.com>
2025-02-23 22:40:27 +0600

    subclass- inh

--- Fundamentals/OOP/Inheritance.swift
+++ Fundamentals/OOP/Inheritance.swift
@@ -9,3 +9,16 @@ class Vehicle {
         print("Speed increased to \(speed) km/h")
     }
 }
+
+// Subclass inheriting from Vehicle
+class Car: Vehicle {
+    var brand: String
+    
+    init(brand: String) {
+        self.brand = brand
+    }
+    
+    func honk() {
+        print("\(brand) goes Beep Beep!")
+    }
+}
