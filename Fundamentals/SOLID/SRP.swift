7e191ddb6ad570fcdc92f5f10778f0036772dfe8
A.F. Adib <a.f.adib36@gmail.com>
2025-02-27 22:17:39 +0600

    Single Responsibility Principle (SRP)

---
+++ Fundamentals/SOLID/SRP.swift
@@ -0,0 +1,11 @@
+
+//Single Responsibility Principle (SRP)
+
+/* A class should have only one reason to change, meaning that a class should have
+only one job or responsibility. This principle helps in keeping the code clean,
+maintainable, and easy to understand.
+*/
+
+import Foundation
+
+// Violation of SRP
