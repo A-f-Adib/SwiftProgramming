6cc22f806bf66e493d603a2338394169738bca52
A.F. Adib <a.f.adib36@gmail.com>
2025-02-28 02:55:31 +0600

    Open-Closed Principle

---
+++ Fundamentals/SOLID/OCP.swift
@@ -0,0 +1,9 @@
+//Open-Closed Principle (OCP)
+
+/* 
+Objects or entities should be open for extension but closed for modification. 
+This means that a class should be easily extendable without modifying the class itself. 
+This principle helps in writing robust and maintainable code.
+*/
+
+import Foundation
