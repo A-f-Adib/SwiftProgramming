660457f16cea89f16036a449aec2480d2f54970e
A.F. Adib <a.f.adib36@gmail.com>
2025-03-05 00:39:34 +0600

    Factory class

--- Fundamentals/DesignPatterns/FactoryPattern.swift
+++ Fundamentals/DesignPatterns/FactoryPattern.swift
@@ -5,6 +5,7 @@ protocol Button {
     func render()
 }
 
+//Concrete Classes
 class iOSButton: Button {
     func render() {
         print("iOS Button Rendered")
@@ -14,5 +15,20 @@ class iOSButton: Button {
 class AndroidButton: Button {
     func render() {
         print("Android Button Rendered")
+    }
+}
+
+
+//Factory class
+class ButtonFactory {
+    static func createButton(for platform: String) -> Button {
+        switch platform {
+        case "iOS":
+            return iOSButton()
+        case "Android":
+            return AndroidButton()
+        default:
+            fatalError("Unsupported platform")
+        }
     }
 }
